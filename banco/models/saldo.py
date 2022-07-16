635ca9bf5dae1ce0bfe118948c4365f7ebea14b8
Stephanie Custodio Santana <100312573+stercustodio@users.noreply.github.com>
2022-07-16 13:39:36 -0300

    Update saldo.py

--- banco/models/saldo.py
+++ banco/models/saldo.py
@@ -3,7 +3,11 @@ from django.db import models
 
 
 class Saldo(models.Model):
-    numero_conta = models.ForeignKey(Conta, on_delete=models.CASCADE)
-    saldo_data = models.DateField()
-    saldo_valor = models.FloatField()
+    conta = models.ForeignKey(Conta, on_delete=models.CASCADE)
+    data_credito = models.DateTimeField()
+    valor_credito = models.FloatField()
+    data_debito = models.DateTimeField()
+    valor_debito = models.FloatField()
+    saldo_final = models.FloatField()
+    
 
