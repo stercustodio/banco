155b51f108c24a5f324c4425ae095c9651d440d3
Stephanie Custodio Santana <100312573+stercustodio@users.noreply.github.com>
2022-07-17 19:57:20 -0300

    Delete banco directory

--- banco/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from banco.models.cliente import Cliente
-from banco.models.conta import Conta
-from banco.models.transferencia import Transferencia
--- banco/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from .cliente_viewset import ClienteViewset
-from .conta_viewset import ContaViewset
-from .saldo_viewset import SaldoViewset
-from .transferencia_viewset import TransferenciaViewset
