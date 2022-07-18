0a801124fdc66d5cd67d462931a10f7800279159
Stephanie Custodio Santana <100312573+stercustodio@users.noreply.github.com>
2022-07-17 22:21:27 -0300

    Update tests.py

--- banco/tests.py
+++ banco/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from banco.models.cliente import Cliente
+
+
+class ClienteTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_criar_cliente(self):
+        # dado
+        Cliente.objects.create(
+            nome="Stephanie Campos Custodio",
+            cpf="150.400.800-50",
+            dt_nascimento="22.08.1994",
+            email="ster@gmail",
+            celular="+5521990047270",
+            senha="123123"
+            )
+        # quando
+        cliente = Cliente.objects.get(
+            nome="Stephanie Campos Custodio"
+            )
+        # entao
+        self.assertEqual(cliente.nome, "Stephanie Campos Custodio") 
