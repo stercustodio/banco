128a64cff928cf7f1a0eaac0249a65d543f315c3
Stephanie Custodio Santana <100312573+stercustodio@users.noreply.github.com>
2022-07-17 21:49:37 -0300

    Update extrato.py

--- banco/models/extrato.py
+++ banco/models/extrato.py
@@ -5,8 +5,11 @@ from django.db import models
 
 
 class Extrato(models.Model):
+    '''Verifica o saldo de uma conta na data especificada'''
+    
     data_consulta = models.DateField(default=date.today)
     conta = models.OneToOneField(Conta, on_delete=models.CASCADE)
     saldo = models.FloatField()
         
-    
+    # não obtive o resultado esperado
+    # deixei salvo algumas tentativas na pasta rascunho    
